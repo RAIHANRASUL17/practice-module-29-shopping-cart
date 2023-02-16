9e856c176bef2bdbf6b7ea62019f54a65a0a1dcc
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-02-17 07:55:40 +0800

    button case number increase

---
+++ js/case-1.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    
+    const caseNumberField= document.getElementById('case-number-field');
+    const previousCaseNumber= parseInt(caseNumberField.value);
+    const newCaseNumber= previousCaseNumber + 1;
+    caseNumberField.value= newCaseNumber;
+})
