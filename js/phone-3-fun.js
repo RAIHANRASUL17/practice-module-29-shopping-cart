14119776eb545da83498e8c92e9f6501906260e3
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-02-17 09:54:48 +0800

    btn phone number plus and minus update function part

---
+++ js/phone-3-fun.js
@@ -0,0 +1,27 @@
+// for btn-phone-plus & minus fuction
+function updatePhoneNumber(isIncrease) {
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const previousPhoneNumber = parseInt(phoneNumberField.value);
+    let newPhoneNumber;
+    if (isIncrease === true) {
+        newPhoneNumber = previousPhoneNumber + 1;
+    }
+    else {
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+    phoneNumberField.value = newPhoneNumber;
+    return newPhoneNumber;
+}
+
+// for btn-phone-plus
+document.getElementById('btn-phone-plus').addEventListener('click', function () {
+    /*call function btn-phone-plus & minus fuction*/
+   const newPhoneNumber= updatePhoneNumber(true)
+})
+
+// for btn-phone-minus
+document.getElementById('btn-phone-minus').addEventListener('click', function () {
+    /*call function btn-phone-plus & minus fuction*/
+    const newPhoneNumber= updatePhoneNumber(false)
+
+})
