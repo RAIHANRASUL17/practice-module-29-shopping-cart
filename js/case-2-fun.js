d443f0922b7ce3f12c21874097a337c855ee0a9b
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-02-17 08:49:31 +0800

    update money with newCaseNumber clicked

--- js/case-2-fun.js
+++ js/case-2-fun.js
@@ -10,8 +10,9 @@ function updateCaseNumber(isIncrese){
     else{
        newCaseNumber= previousCaseNumber - 1;
     }
-    
     caseNumberField.value= newCaseNumber;
+
+    return newCaseNumber;
 }
 
 
@@ -19,12 +20,21 @@ function updateCaseNumber(isIncrese){
 document.getElementById('btn-case-plus').addEventListener('click', function(){
 
     /*call function*/ 
-    updateCaseNumber(true);
+   const newCaseNumber= updateCaseNumber(true);
+    /*update money newCaseNumber*/ 
+    const caseTotalPrice= newCaseNumber * 59;
+    const caseTotalElement= document.getElementById('case-total');
+    caseTotalElement.innerText= caseTotalPrice;
 });
 
 // for bnt case-minus
 document.getElementById('btn-case-minus').addEventListener('click', function(){
 
     /*call function*/ 
-    updateCaseNumber(false);
+   const newCaseNumber= updateCaseNumber(false);
+
+        /*update money newCaseNumber*/ 
+        const caseTotalPrice= newCaseNumber * 59;
+        const caseTotalElement= document.getElementById('case-total');
+        caseTotalElement.innerText= caseTotalPrice;
 })
