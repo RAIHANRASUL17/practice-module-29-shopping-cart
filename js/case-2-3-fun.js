7d1f53fd31ca5632d9d4dc4803f05e1998acecc2
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-02-17 09:10:55 +0800

    update money with newCaseNumber clicked function

---
+++ js/case-2-3-fun.js
@@ -0,0 +1,40 @@
+// btn-case-plus and btn-case-minus function
+function updateCaseNumber(isIncrese) {
+    const caseNumberField = document.getElementById('case-number-field');
+    const previousCaseNumber = parseInt(caseNumberField.value);
+    // const newCaseNumber= previousCaseNumber + 1;
+    let newCaseNumber;
+    if (isIncrese === true) {
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else {
+        newCaseNumber = previousCaseNumber - 1;
+    }
+    caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber;
+}
+
+// updateCaseTotalPrice function
+function updateCaseTotalPrice(newCaseNumber){
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+}
+
+
+// for btn-case-plus
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+    /*call function*/
+    const newCaseNumber = updateCaseNumber(true);
+    /* call function updateCaseTotalPrice*/
+    updateCaseTotalPrice(newCaseNumber);
+});
+
+// for bnt case-minus
+document.getElementById('btn-case-minus').addEventListener('click', function () {
+    /*call function*/
+    const newCaseNumber = updateCaseNumber(false);
+     /* call function updateCaseTotalPrice*/
+     updateCaseTotalPrice(newCaseNumber);
+})
